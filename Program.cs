4d408b91480118ef8df9f4f94cd910d52f9e09e3
Stevvvvvi <lly363893886@gmail.com>
2022-03-04 22:12:26 +1100

    read file

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Start App!");
+int counter = 0;  
+var path = Path.Combine(Directory.GetCurrentDirectory(), "poker-hands.txt");
+Console.WriteLine(path);
+// Read the file and display it line by line.  
+foreach (string line in System.IO.File.ReadLines(path))
+{  
+    System.Console.WriteLine(line);  
+    counter++;  
+}  
+  
+System.Console.WriteLine("There were {0} lines.", counter); 
