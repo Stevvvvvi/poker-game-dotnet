4c7e0ab19ad093e09542cfa56aa585dfb4be6c1a
Stevvvvvi <lly363893886@gmail.com>
2022-03-06 14:13:03 +1100

    finish logic

--- Contract/HandCard.cs
+++ Contract/HandCard.cs
@@ -7,41 +7,122 @@ namespace poker_game_dotnet.Contract
 {
     public class HandCard
     {
-        public List<string> currentHand = new List<string>();
-        private int rank;
+        public List<string> CurrentHand { get; private set; } = new List<string>();
+        public int HandRank { get; private set; } = 1;
+        public char HighCardInRank { get; private set; } = '2';
+        public char SecondaryHighCardInRank { get; private set; } = '2';
 
-        private IEnumerable<char> suits = new List<char>(){'C', 'D', 'H', 'S'};
-        private IEnumerable<char> cardRanks = new List<char>(){
+        public IEnumerable<char> Suits { get; private set; } = new List<char>(){'C', 'D', 'H', 'S'};
+        public IEnumerable<char> CardRanks { get; private set; } = new List<char>(){
             '2','3','4','5','6','7','8','9','T','J','Q','K','A'
         };
         public HandCard(IEnumerable<string> inputHand)
         {
             // we sort the card based on number first
-            foreach(var cardRank in cardRanks){
+            foreach(var cardRank in CardRanks){
                 foreach(var currentInputCard in inputHand){
                     if (cardRank == currentInputCard.ToCharArray()[0]){
-                        this.currentHand.Add(currentInputCard);
+                        this.CurrentHand.Add(currentInputCard);
                     }
                 }
             }
+
+            EvaluateHand();
         }
         private bool IsFlush(){
-            //get all suits
-            var handSuits = currentHand.Select(e=>e.ToCharArray()[1]);
-            var EachTypesOfSuit = new Dictionary<char,int>();
-            foreach(var eachSuit in suits){
-                EachTypesOfSuit[eachSuit]++;
-            }
+            //get all Suits
+            var EachTypesOfSuit= GetEachSuitAmount();
             if (EachTypesOfSuit.Any(e=>e.Value==5)){
                 return true;
             }else{
                 return false;
             }
         }
+        private Dictionary<char, int> GetEachSuitAmount(){
+            var handSuits = CurrentHand.Select(e=>e.ToCharArray()[1]);
+            var EachTypesOfSuit = new Dictionary<char,int>();
+            foreach(var suit in handSuits.Distinct()){
+                EachTypesOfSuit.Add(suit, 0);
+            }
+            foreach(var eachSuit in handSuits){
+                EachTypesOfSuit[eachSuit]++;
+            }
+            return EachTypesOfSuit;
+        }
+        private Dictionary<char, int> GetEachCardAmount(){
+            // get all card number
+            var handCardNumbers = CurrentHand.Select(e=>e.ToCharArray()[0]);
+            var EachTypesOfCard = new Dictionary<char, int>();
+            foreach(var card in handCardNumbers.Distinct()){
+                EachTypesOfCard.Add(card, 0);
+            }
+            foreach(var cardRank in handCardNumbers){
+                EachTypesOfCard[cardRank]++;
+            }
+            return EachTypesOfCard;
+        }
+        private string IsStraight(){
+            var startCardIndex = CardRanks.ToList().FindIndex(e=>e==CurrentHand[0].ToCharArray()[0]);
+            var cardStraightRank = String.Join("",CardRanks.ToList().Skip(startCardIndex).Take(5));
+            var currentHandCardConcat = String.Join("", this.CurrentHand.Select(e=>e.ToCharArray()[0]));
+            if (currentHandCardConcat=="TJQKA" && currentHandCardConcat == cardStraightRank){
+                return "royalStraight";
+            }else if (currentHandCardConcat==cardStraightRank){
+                return "straight";
+            }else{
+                return "notStraight";
+            }
+        }
+        private int GetHowManyPairs(){
+            var eachCardAmount = GetEachCardAmount();
+            var pairAmount = eachCardAmount.Where(e=>e.Value == 2).Count();
+            return pairAmount;
+        }
 
         public void EvaluateHand(){
-            // search for royal flush
-
+            var eachCardAmount = GetEachCardAmount();
+            // search for royalFlush
+            if (IsFlush() && IsStraight()=="royalStraight"){
+                HandRank = 10;
+            }else if (IsFlush() && IsStraight() == "straight"){
+                // straight Flush
+                HandRank = 9;
+                this.HighCardInRank = CurrentHand.Last().ToCharArray()[0];
+            }else if (eachCardAmount.Any(e=>e.Value==4)){
+                // Four of kind
+                HandRank = 8;
+                this.HighCardInRank = eachCardAmount.First(e=>e.Value==4).Key;
+            }else if (eachCardAmount.Any(e=>e.Value==3) && GetHowManyPairs() == 1){
+                // Full house
+                HandRank = 7;
+                this.HighCardInRank = eachCardAmount.First(e=>e.Value==3).Key;
+                this.SecondaryHighCardInRank = eachCardAmount.First(e=>e.Value==2).Key;
+            }else if (IsFlush()==true){
+                // flush
+                HandRank = 6;
+                this.HighCardInRank = CurrentHand.Last().ToCharArray()[0];
+            }else if (IsStraight() == "straight"){
+                // straight
+                HandRank = 5;
+                this.HighCardInRank = CurrentHand.Last().ToCharArray()[0];
+            }else if (eachCardAmount.Any(e=>e.Value==3)){
+                //Three of a Kind
+                HandRank = 4;
+                this.HighCardInRank = eachCardAmount.First(e=>e.Value==3).Key;
+            }else if (GetHowManyPairs()==2){
+                // 2 pairs
+                HandRank = 3;
+                var pairAmount = eachCardAmount.Where(e=>e.Value == 2);
+                this.HighCardInRank = pairAmount.Last().Key;
+                this.SecondaryHighCardInRank = pairAmount.First().Key;
+            }else if (GetHowManyPairs()==1){
+                // 1 pair
+                HandRank = 2;
+                this.HighCardInRank = eachCardAmount.First(e=>e.Value==2).Key;
+            }else{
+                HandRank = 1;
+                this.HighCardInRank = CurrentHand.Last().ToCharArray()[0];
+            }
         }
         
     }
--- Program.cs
+++ Program.cs
@@ -3,6 +3,9 @@ using poker_game_dotnet.Contract;
 
 Console.WriteLine("Start App!");
 int counter = 0;  
+int firstPlayerWinCount = 0;
+int secondPlayerWinCount = 0;
+int tie = 0;
 var path = Path.Combine(Directory.GetCurrentDirectory(), "poker-hands.txt");
 
 // Read the file and display it line by line.  
@@ -13,9 +16,56 @@ foreach (string line in System.IO.File.ReadLines(path))
     HandCard FirstPlayer = new HandCard(firstPlayerHand);
     var secondPlayerHand = allCards.Skip(5).Take(5);
     HandCard SecondPlayer = new HandCard(secondPlayerHand);
-    Console.WriteLine("firstPlayerHand: "+String.Join(" ", FirstPlayer.currentHand));
-    Console.WriteLine("secondPlayerHand: "+ String.Join(" ", SecondPlayer.currentHand));
+    Console.WriteLine("firstPlayerHand: "+String.Join(" ", FirstPlayer.CurrentHand));
+    Console.WriteLine("firstPlayerRank: "+String.Join(" ", FirstPlayer.HandRank));
+    Console.WriteLine("secondPlayerHand: "+ String.Join(" ", SecondPlayer.CurrentHand));
+    Console.WriteLine("secondPlayerRank: "+String.Join(" ", SecondPlayer.HandRank));
+
+    //compare 2 players rank
+    if (FirstPlayer.HandRank > SecondPlayer.HandRank){
+        firstPlayerWinCount++;
+    }else if (FirstPlayer.HandRank < SecondPlayer.HandRank){
+        secondPlayerWinCount++;
+    }else if (FirstPlayer.HandRank == SecondPlayer.HandRank){
+        // 2 players rank the same, compare rank high card first
+        var firstPlayerHighCardInRankIndex = FirstPlayer.CardRanks.ToList().IndexOf(FirstPlayer.HighCardInRank);
+        var secondPlayerHighCardInRankIndex = SecondPlayer.CardRanks.ToList().IndexOf(SecondPlayer.HighCardInRank);
+        if ( firstPlayerHighCardInRankIndex > secondPlayerHighCardInRankIndex){
+            firstPlayerWinCount++;
+        }else if (firstPlayerHighCardInRankIndex < secondPlayerHighCardInRankIndex){
+            secondPlayerWinCount++;
+        }else if (firstPlayerHighCardInRankIndex == secondPlayerHighCardInRankIndex){
+            // 2 players rank the same in card rank high card too
+            var firstPlayerSecondaryHighCardInRank = FirstPlayer.CardRanks.ToList().IndexOf(FirstPlayer.SecondaryHighCardInRank);
+            var secondPlayerSecondaryHighCardInRank = SecondPlayer.CardRanks.ToList().IndexOf(SecondPlayer.SecondaryHighCardInRank);
+            if (firstPlayerSecondaryHighCardInRank > secondPlayerSecondaryHighCardInRank){
+                firstPlayerWinCount++;
+            }else if (firstPlayerSecondaryHighCardInRank < secondPlayerSecondaryHighCardInRank){
+                secondPlayerWinCount++;
+            }else if (firstPlayerSecondaryHighCardInRank == secondPlayerSecondaryHighCardInRank){
+                // compare all cards from high to low, convert card high to index for compare
+                var player1CardsIndex = FirstPlayer.CurrentHand.Select(e=>FirstPlayer.CardRanks.ToList().IndexOf(e.ToCharArray()[0]));
+                var player2CardsIndex = SecondPlayer.CurrentHand.Select(e=>SecondPlayer.CardRanks.ToList().IndexOf(e.ToCharArray()[0]));
+                // convert card high to index for compare
+                var player1NotInPlayer2Index = player1CardsIndex.Except(player2CardsIndex).ToList();
+                var player2NotInPlayer1Index = player2CardsIndex.Except(player1CardsIndex).ToList();
+                if (player1NotInPlayer2Index.Last() > player2NotInPlayer1Index.Last()){
+                    firstPlayerWinCount++;
+                }else if (player1NotInPlayer2Index.Last() < player2NotInPlayer1Index.Last()){
+                    secondPlayerWinCount++;
+                }else {
+                    // 2 player has exact same cards
+                    System.Console.WriteLine("2 Players have exact same card"); 
+                    tie++;
+                }
+            }
+        }
+    }
     counter++;
 }  
-  
-System.Console.WriteLine("There were {0} lines.", counter); 
+System.Console.WriteLine("There were {0} lines..........................................", counter);   
+System.Console.WriteLine("_________________________________"); 
+System.Console.WriteLine("FirstPlayerWin: {0}", firstPlayerWinCount); 
+System.Console.WriteLine("SecondPlayerWin: {0}", secondPlayerWinCount); 
+System.Console.WriteLine("2 players tie: {0}", tie); 
+System.Console.WriteLine("_________________________________"); 
